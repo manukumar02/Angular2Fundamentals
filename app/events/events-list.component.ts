7efeff7b5365be9ca3f85df4641f62f5617b84dc
Manu Sharma <manukumar02@gmail.com>
2017-07-09 18:39:58 +0530

    [External plugin] - Added toastr as injectable service.

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,32 +1,36 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { EventService } from './shared/event.service'
+import { ToastrService } from '../common/toastr.service'
 
 @Component({
     selector: 'events-list',
     template: `
         <div>
             <h1>Upcoming Angular2 Events</h1>
-            <hr> 
-            <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+            <hr>
+            <div class="row">
+                <div *ngFor="let event of events" class="col-md-5">
+                    <event-thumbnail (click)="handleThumbnailClick(event.name)" #thumbnail [event]="event"></event-thumbnail>
+                </div>                
+            </div>            
             <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me Foo</button>      
         </div>
     `
 })
 
-export class EventsListComponent {
-    event1 = {
-        id: 1,
-        name: 'Angular Connect',
-        date: new Date(),
-        time: '10:00 am',
-        price: 599.99,
-        imageUrl: '/app/assets/images/angularconnect-shield.png',
-        location: {
-            address: '1507 DT',
-            city: 'Bangalore',
-            country: 'India'
-        }
+export class EventsListComponent implements OnInit{
+    events:any[];
+    constructor(private eventService: EventService, private toastr: ToastrService) {
+        //this.events = this.eventService.getEvents();
     }
 
+    ngOnInit() {
+        this.events = this.eventService.getEvents();
+    }
+    
+    handleThumbnailClick(eventName) {
+        this.toastr.success(eventName);
+    }
     // handleEventClicked(data) {
     //     console.log('Recieved ', data)
     // }
--- app/events/events-thumbnails.component.ts
+++ app/events/events-thumbnails.component.ts
@@ -4,17 +4,32 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
             <div class="well hoverwell thumbnail">
-                <h2>{{event.name}} </h2>
-                <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
-                <div>Price: \${{event.price}}</div>
-                <div>
-                    <span>Location: {{event.location.address}}</span>
+                <h2>{{event?.name}} </h2>
+                <div>Date: {{event?.date}}</div>
+                <div [ngClass]="getStatTimeClass()" [ngSwitch]="event?.time">
+                    Time: {{event?.time}}
+                    <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                    <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                    <span *ngSwitchDefault>(Normal Start)</span>
+                </div>
+                <div>Price: \${{event?.price}}</div>
+                <div *ngIf="event?.location">
+                    <span>Location: {{event?.location?.address}}</span>
                     <span>&nbsp;</span>
-                    <span>{{event.location.city}}, {{event.location.country}}</span>
+                    <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
+                </div>
+                <div *ngIf="event?.onlineUrl">
+                    <div>Online Url: {{event?.onlineUrl}}</div>
                 </div>                
             </div>
-        `
+        `,
+        styles: [`
+            .green {color: green !important;}
+            .bold {font-weight: bold;}
+            .thumbnail {min-height: 210px;}
+            .pad-left {margin-left: 10px; }
+            .well div {color: #bbb;}
+        `]
 })
 
 export class EventThumbnailComponent {
@@ -25,7 +40,18 @@ export class EventThumbnailComponent {
     //     this.eventClick.emit(this.event)
     // }
 
-    logFoo() {
-        console.log('Log Foo');
+    getStatTimeClass() {
+        // const isEarlyStart = this.event && this.event.time === '8:00 am';
+        // return {green: isEarlyStart, bold: isEarlyStart}
+        // if(this.event && this.event.time === '8:00 am') {
+        //     return 'green bold'
+        // } else {
+        //     return ''
+        // }
+        if(this.event && this.event.time === '8:00 am') {
+            return ['green', 'bold']
+        } else {
+            return []
+        }
     } 
 }
