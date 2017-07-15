2c94eecfe7cd25b69ba6eeaa060c4baa4dad9b43
Manu Sharma <manukumar02@gmail.com>
2017-07-15 22:57:57 +0530

    [Login] - Added the new login page - in progress

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/events-thumbnails.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './router'
-import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './error/404.component'
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import {EventListResolver} from './events/events-list-resolver.service'
+
 @NgModule({
     imports: [
         BrowserModule,
@@ -42,10 +46,10 @@ import {EventListResolver} from './events/events-list-resolver.service'
 export class AppModule { }
 
 function checkDirtyState(component: CreateEventComponent) {
-    if(component.isDirty) {
+    if (component.isDirty) {
         return window.confirm('You have not saved this event, do you really want to cancel?');
     } else {
         return true;
     }
-    
+
 }
